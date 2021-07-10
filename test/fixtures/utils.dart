9f4dae0e6fe07370c9cc751a0e89313c3c639e32
Mohammed Almissbah <mohammed.almissbah@hotmail.com>
2021-07-10 20:15:33 +0400

    add teat fixtures.

--- test/fixtures/utils.dart
+++ test/fixtures/utils.dart
@@ -1,10 +1,15 @@
  
 import 'package:metaweather_client/features/forecast/data/models/forecast_api_response.dart';
+import 'package:metaweather_client/features/forecast/data/models/weather_forecast_model.dart';
 import 'package:metaweather_client/features/forecast/domain/entities/weather_forecast.dart';
 
 class TestUtils {
   static WeatherForecast getWeatherForecast() {
     return WeatherForecast(daysForecasts: [],locationName:'London' ,locationType:'City' );
+  }
+
+ static WeatherForecastModel getWeatherForecastModel() {
+    return WeatherForecastModel(daysForecasts: [],locationName:'London' ,locationType:'City' );
   }
 
     static ForecastApiResponse getForecastApiResponse() {
