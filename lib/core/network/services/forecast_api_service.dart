1a3105e83110ac6d81676f508fcc915675fb7184
Mohammed Almissbah <mohammed.almissbah@hotmail.com>
2021-07-10 20:24:00 +0400

    add API service.

---
+++ lib/core/network/services/forecast_api_service.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:metaweather_client/features/forecast/data/models/forecast_api_response.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart'; 
+part 'forecast_api_service.g.dart';
+
+@RestApi(baseUrl: "https://www.metaweather.com/api")
+abstract class ForecastApiService {
+  factory ForecastApiService(Dio dio) = _ForecastApiService;
+ 
+  @GET("/location/search/{woeid}")
+  Future<HttpResponse<ForecastApiResponse>> fetchWeatherForecast(
+      {@required @Path("woeid") String woeid, });
+}
---
+++ lib/core/network/services/forecast_api_service.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'forecast_api_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _ForecastApiService implements ForecastApiService {
+  _ForecastApiService(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+    baseUrl ??= 'https://www.metaweather.com/api';
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  Future<HttpResponse<ForecastApiResponse>> fetchWeatherForecast(
+      {woeid}) async {
+    ArgumentError.checkNotNull(woeid, 'woeid');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    final _result = await _dio.request<Map<String, dynamic>>(
+        '/location/search/$woeid',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ForecastApiResponse.fromJson(_result.data);
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+}
