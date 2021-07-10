b173e85980b177af6aff9f7b8cab9656993124fe
Mohammed Almissbah <mohammed.almissbah@hotmail.com>
2021-07-10 18:47:22 +0400

    add test fixtures.

---
+++ test/fixtures/utils.dart
@@ -0,0 +1,14 @@
+ 
+import 'package:metaweather_client/features/forecast/data/models/forecast_api_response.dart';
+import 'package:metaweather_client/features/forecast/domain/entities/weather_forecast.dart';
+
+class TestUtils {
+  static WeatherForecast getWeatherForecast() {
+    return WeatherForecast(daysForecasts: [],locationName:'London' ,locationType:'City' );
+  }
+
+    static ForecastApiResponse getForecastApiResponse() {
+    return ForecastApiResponse(daysForecast: [],locationName:'London' ,locationType:'City' );
+  }
+     
+}
